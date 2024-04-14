e0a83fbce889797ef93240703c10cae3e90f0ba2
Jakub Jabłoński <jakub.jabloo@gmail.com>
2024-04-14 21:03:21 +0200

    Fix backend structure

---
+++ backend/src/main/java/jablonski/jakub/BookYou/BookYouApplication.java
@@ -0,0 +1,13 @@
+package jablonski.jakub.BookYou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookYouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookYouApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/jablonski/jakub/BookYou/BookYouApplicationTests.java
@@ -0,0 +1,13 @@
+package jablonski.jakub.BookYou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookYouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
