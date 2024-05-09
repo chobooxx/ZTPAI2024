c961caeab37cf553173df0211135c95465623e54
Jakub Jablonski <jakub.jabloo@gmail.com>
2024-05-10 00:03:00 +0200

    finish jwt authentication

---
+++ backend/src/main/java/jablonski/jakub/BookYou/controller/AuthenticationController.java
@@ -0,0 +1,34 @@
+package jablonski.jakub.BookYou.controller;
+
+import jablonski.jakub.BookYou.dto.AuthenticationRequest;
+import jablonski.jakub.BookYou.dto.AuthenticationResponse;
+import jablonski.jakub.BookYou.dto.RegisterRequest;
+import jablonski.jakub.BookYou.service.AuthenticationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    private final AuthenticationService service;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(
+        @RequestBody RegisterRequest request
+    ) {
+        return ResponseEntity.ok(service.register(request));
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticate(
+            @RequestBody AuthenticationRequest request
+    ) {
+        return ResponseEntity.ok(service.authenticate(request));
+    }
+}
---
+++ backend/src/main/java/jablonski/jakub/BookYou/service/AuthenticationService.java
@@ -0,0 +1,54 @@
+package jablonski.jakub.BookYou.service;
+
+import jablonski.jakub.BookYou.dto.AuthenticationRequest;
+import jablonski.jakub.BookYou.dto.AuthenticationResponse;
+import jablonski.jakub.BookYou.dto.RegisterRequest;
+import jablonski.jakub.BookYou.model.User;
+import jablonski.jakub.BookYou.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .name(request.getName())
+                .surname(request.getSurname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .types(new HashSet<>())
+                .build();
+        repository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        var user = repository.findByEmail(request.getEmail())
+                .orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+}
